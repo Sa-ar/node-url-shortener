12eac6ac43eddeeafd99456f1e182309250147f7
Saar Davidson <41952193+Sa-ar@users.noreply.github.com>
2020-05-30 18:40:17 +0300

    add semicolons

--- server.js
+++ server.js
@@ -1,43 +1,43 @@
-const express = require('express')
-const dotenv = require('dotenv')
-dotenv.config()
-const mongoose = require('mongoose')
-const ShortUrl = require('./shortUrl')
-const app = express()
+const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
+const mongoose = require('mongoose');
+const ShortUrl = require('./shortUrl');
+const app = express();
 
 mongoose.connect(process.env.CONNECTIONSTRING, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+  useUnifiedTopology: true,
+});
 
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 app.use(express.urlencoded({
-  extended: false
-}))
+  extended: false,
+}));
 
 app.get('/', async (req, res) => {
-  const shortUrls = await ShortUrl.find()
+  const shortUrls = await ShortUrl.find();
   res.render('index', {
-    shortUrls: shortUrls
-  })
-})
+    shortUrls: shortUrls,
+  });
+});
 
 app.post('/shortUrls', async (req, res) => {
   await ShortUrl.create({
-    full: req.body.fullUrl
-  })
+    full: req.body.fullUrl,
+  });
 
-  res.redirect('/')
-})
+  res.redirect('/');
+});
 
 app.get('/:shortUrl', async (req, res) => {
   const shortUrl = await ShortUrl.findOne({
-    short: req.params.shortUrl
-  })
-  if (shortUrl == null) return res.sendStatus(404)
+    short: req.params.shortUrl,
+  });
+  if (shortUrl == null) return res.sendStatus(404);
 
-  shortUrl.clicks++
-  shortUrl.save(() => res.redirect(shortUrl.full))
-})
+  shortUrl.clicks++;
+  shortUrl.save(() => res.redirect(shortUrl.full));
+});
 
-app.listen(process.env.PORT || 5000)
+app.listen(process.env.PORT || 5000);
